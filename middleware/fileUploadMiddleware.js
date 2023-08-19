e81c1fc8ef6732f9ba77a85f1b5be5bc258672a7
Abhishek Kumar <72213318+AK-ABHISHEK-KUMAR@users.noreply.github.com>
2023-08-19 14:04:22 +0530

    Update fileUploadMiddleware.js

--- middleware/fileUploadMiddleware.js
+++ middleware/fileUploadMiddleware.js
@@ -1,8 +1,15 @@
+import fs from "fs";
 import multer from "multer";
 
 const storageConfig = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    const uploadPath = "uploads/";
+
+    // Create the folder if it doesn't exist
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath);
+    }
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     const name = Date.now() + "-" + file.originalname;
